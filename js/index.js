ef18116f7b1d527c18f8a593045d54b61f5ef1b3
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-03 11:36:55 +0100

    :sparkles: Add mouseover event to nav items
    Use a query selector all for all .nav-links and change their color on mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+
+document.querySelectorAll('.nav-link').forEach(navItem => {
+    navItem.addEventListener('mouseover', (event) => { event.target.style.color = 'gold' })
+})
