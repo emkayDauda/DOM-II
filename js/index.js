0ba8d1d1d6892bf1bc21736760cd7c1d87263a99
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-03 11:50:29 +0100

    :sparkles: ContextMenu event
    Add a context menu event disallowing right-click on all images

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ document.querySelectorAll('.nav-link').forEach(navItem => {
     navItem.addEventListener('mouseover', (event) => { event.target.style.color = 'gold' })
     navItem.addEventListener('mouseout', (event) => { event.target.style.color = 'silver' })
 })
+
+Array.from(document.getElementsByTagName('img')).forEach(element => {
+    element.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        alert('NOT ALLOWEDDDD!!!!')
+})
+})
